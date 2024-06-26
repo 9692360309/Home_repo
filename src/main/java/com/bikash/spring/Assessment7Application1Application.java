8fd85fb9d88318c299a6731428a608cc48ea7767
9692360309 <bikash.pattnaik14@gmail.com>
2024-06-26 16:14:26 +0530

    assessment

---
+++ src/main/java/com/bikash/spring/Assessment7Application1Application.java
@@ -0,0 +1,13 @@
+package com.bikash.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assessment7Application1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assessment7Application1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikash/spring/Assessment7Application1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikash.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assessment7Application1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
